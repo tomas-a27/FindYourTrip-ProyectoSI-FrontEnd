e20f8bed88a59f61e9abe37d84c3fec20e82f9a1
Lucila Vega <lucilavega45@gmail.com>
2026-02-23 15:36:32 -0300

    agrego algunas entidades

--- src/entities/entities.ts
+++ src/entities/entities.ts
@@ -7,4 +7,46 @@ interface LocalidadDTO extends BaseDTO {
   nombre: string;
 }
 
-export type { LocalidadDTO };
+interface UsuarioDTO {
+  idUsuario: number;
+  tipoUsuario: string;
+  nombreUsuario: string;
+  apellidoUsuario: string;
+  tipoDocumento: string;
+  nroDocumento: string;
+  email: string;
+  telefono: string;
+  contrasenaUsuario: string;
+  generoUsuario: string;
+  calificacionPas?: number;
+  estadoUsuario: string;
+  nroLicenciaConductorUsuario?: string;
+  vigenciaLicenciaConductorUsuario?: Date;
+  fotoLicenciaConductorUsuario?: string;
+  calificacionConductor?: number;
+  estadoConductor?: string;
+  fotoPerfil?: string;
+}
+
+interface ViajeDTO {
+  viajeId: number;
+  viajeOrigen: LocalidadDTO;
+  viajeDestino: LocalidadDTO;
+  viajeFecha: Date;
+  viajeHorario: string;
+  viajeCantLugares: number;
+  viajeEstado: string;
+  viajeComentario?: string;
+  viajeAceptaMascotas: boolean;
+  viajePrecio: number;
+}
+
+interface VehiculoDTO {
+  patente: string;
+  modelo: string;
+  cantLugares: number;
+  color: string;
+  marca: string;
+}
+
+export type { LocalidadDTO, UsuarioDTO, ViajeDTO, VehiculoDTO };
