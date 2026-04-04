ff920940d140677b37d534a93f270b5451af894b
meliaronson <melinaaronson@gmail.com>
2026-04-04 17:42:30 -0300

    eliminar opcion de prefiero no decirlo

--- src/components/Usuario/CrearUsuario.tsx
+++ src/components/Usuario/CrearUsuario.tsx
@@ -193,9 +193,6 @@ export const CrearUsuario = () => {
                     <option value="Femenino">Femenino</option>
                     <option value="Masculino">Masculino</option>
                     <option value="Otro">Otro</option>
-                    <option value="Prefiero no decirlo">
-                      Prefiero no decirlo
-                    </option>
                   </select>
                 </div>
               </div>
